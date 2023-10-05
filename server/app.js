90e9630f51a042bc4748c2d6a1befe32b5d38622
kml562 <111355794+kml562@users.noreply.github.com>
2023-10-05 10:08:45 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 import express from "express";
-import cors from 'cors'
+import cors from 'cors';
+import http from 'http';
+import { Server } from "socket.io";
 import userRouter from './src/routes/userRouter.js'
 const app= express();
 
@@ -9,7 +11,34 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
+const server= http.createServer(app);
+const io= new Server(server,{
+    cors:{
+        origin:`http://localhost:3000`,
+        methods:["GET","POST"],
+    }
+});
 
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+  
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+  });
+  server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+  });
+  
 // ---------------------------------------------------------------
 app.use('/user', userRouter);
 
