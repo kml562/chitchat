94febe8e18049956317ff7ba7333c1d8017d5c4b
kml562 <111355794+kml562@users.noreply.github.com>
2023-10-05 07:30:57 +0530

    doen with new update

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import io from 'socket.io-client';
+ io.connect("http://localhost:3001/");
+function App() {
+  return (
+   <>
+   </>
+  );
+}
+
+export default App;
--- src/utils/bcrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-export const hashPasswordfun= async(pass)=>{
-  
-        const salt= 10;
-        const hashedPassword=   await bcrypt.hash(pass, salt);
-        return hashedPassword
-   
-}
